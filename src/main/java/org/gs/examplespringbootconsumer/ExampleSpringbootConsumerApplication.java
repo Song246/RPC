a2a4257dd785223200fd224e010384744c0f1187
tckry <1150590475@qq.com>
2024-04-26 15:37:25 +0800

    feature：Springboot starter 开发

---
+++ src/main/java/org/gs/examplespringbootconsumer/ExampleSpringbootConsumerApplication.java
@@ -0,0 +1,15 @@
+package org.gs.examplespringbootconsumer;
+
+import com.gs.rpc.springboot.starter.EnableRpc;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableRpc(needServer = false)
+public class ExampleSpringbootConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleSpringbootConsumerApplication.class, args);
+    }
+
+}
